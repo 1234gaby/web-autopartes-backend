d580a5dd8488bfcb4321a20aadc8dd341e02391a
1234gaby <gos200395@gmail.com>
2025-04-20 23:21:16 -0300

    Forzar deploy para Render

--- database.js
+++ database.js
@@ -2,7 +2,7 @@
 const Database = require('better-sqlite3');
 const db = new Database('autopartes.db');
 
-// Crear tabla si no existe
+// Crear tabla de usuarios si no existe
 db.prepare(`
   CREATE TABLE IF NOT EXISTS users (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -11,4 +11,25 @@ db.prepare(`
   )
 `).run();
 
-module.exports = { db }; // 👈 así lo exportás como objeto con clave "db"
+// Crear tabla de publicaciones si no existe
+db.prepare(`
+  CREATE TABLE IF NOT EXISTS publicaciones (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    nombre_producto TEXT,
+    marca TEXT,
+    modelo TEXT,
+    precio REAL,
+    ubicacion TEXT,
+    envio TEXT,
+    tipo_envio TEXT,
+    categoria TEXT,
+    estado TEXT,
+    codigo_serie TEXT,
+    compatibilidad TEXT,
+    marca_repuesto TEXT,
+    fotos TEXT,
+    user_id INTEGER
+  )
+`).run();
+
+module.exports = db;
